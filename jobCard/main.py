e1afde161cf53097e4789580804bf55971a90a64
jiqi963 <jiqi963@gmail.com>
2021-03-17 11:15:48 +1300

    added second cols, need add some space between two tables

--- jobCard/main.py
+++ jobCard/main.py
@@ -1,4 +1,4 @@
-# import library
+# import libraries
 from docx import Document
 from docx.enum.section import WD_ORIENTATION
 from docx.shared import Inches
@@ -18,31 +18,66 @@ section.page_height = page_h
 section.left_margin, section.right_margin = Inches(0.2), Inches(0.2)
 section.top_margin, section.bottom_margin = Inches(0.2), Inches(0.2)
 
-# section.sectPr.xpath('/w:cols')[0].set(qn('w:num'), '2')
+jobNumber = str(4000)
 
-table = document.add_table(rows=12, cols=2, style='Table Grid')
+table = document.add_table(rows=13, cols=2, style='Table Grid')
 
-table.cell(1, 0).merge(table.cell(2, 0))
-table.cell(3, 0).merge(table.cell(5, 0))
-table.cell(7, 0).merge(table.cell(8, 0))
-table.cell(9, 0).merge(table.cell(11, 0))
+table.cell(2, 0).merge(table.cell(3, 0))
+table.cell(4, 0).merge(table.cell(6, 0))
+table.cell(8, 0).merge(table.cell(9, 0))
+table.cell(10, 0).merge(table.cell(12, 0))
+table.cell(0, 0).merge(table.cell(0, 1))
 
-table.cell(0, 1).merge(table.cell(2, 1))
-table.cell(3, 1).merge(table.cell(5, 1))
-table.cell(6, 1).merge(table.cell(8, 1))
+table.cell(0, 0).text = "Job Number " + jobNumber
+table.cell(1, 0).text = "Date:"
+table.cell(3, 0).text = "Client:"
+table.cell(5, 0).text = "Phone & Email:"
+table.cell(7, 0).text = "Password:"
+table.cell(9, 0).text = "Parts:"
+table.cell(10, 0).text = "Items Serviced:"
 
-paragraph = document.add_paragraph()
-paragraph_format = paragraph.paragraph_format
-paragraph_format.space_before = Pt(18)
+table.cell(1, 1).merge(table.cell(3, 1))
+table.cell(4, 1).merge(table.cell(6, 1))
+table.cell(7, 1).merge(table.cell(9, 1))
+table.cell(11, 1).merge(table.cell(12, 1))
 
-table = document.add_table(rows=12, cols=2, style='Table Grid')
-table.cell(1, 0).merge(table.cell(2, 0))
-table.cell(3, 0).merge(table.cell(5, 0))
-table.cell(7, 0).merge(table.cell(8, 0))
-table.cell(9, 0).merge(table.cell(11, 0))
+table.cell(1, 1).text = "Address:"
+table.cell(4, 1).text = "Issue:"
+table.cell(7, 1).text = "Work Done:"
+table.cell(10, 1).text = "Misc Notes:"
+table.cell(11, 1).text = "To Invoice:"
 
-table.cell(0, 1).merge(table.cell(2, 1))
-table.cell(3, 1).merge(table.cell(5, 1))
-table.cell(6, 1).merge(table.cell(8, 1))
+# Add a new cols
+section._sectPr.xpath('./w:cols')[0].set(qn('w:num'), '2')
+
+paragraph_format.space_before, paragraph_format.space_after
+(None, None)
+paragraph_format.space_before
+table = document.add_table(rows=13, cols=2, style='Table Grid')
+
+table.cell(2, 0).merge(table.cell(3, 0))
+table.cell(4, 0).merge(table.cell(6, 0))
+table.cell(8, 0).merge(table.cell(9, 0))
+table.cell(10, 0).merge(table.cell(12, 0))
+table.cell(0, 0).merge(table.cell(0, 1))
+
+table.cell(0, 0).text = "Job Number " + jobNumber
+table.cell(1, 0).text = "Date:"
+table.cell(3, 0).text = "Client:"
+table.cell(5, 0).text = "Phone & Email:"
+table.cell(7, 0).text = "Password:"
+table.cell(9, 0).text = "Parts:"
+table.cell(10, 0).text = "Items Serviced:"
+
+table.cell(1, 1).merge(table.cell(3, 1))
+table.cell(4, 1).merge(table.cell(6, 1))
+table.cell(7, 1).merge(table.cell(9, 1))
+table.cell(11, 1).merge(table.cell(12, 1))
+
+table.cell(1, 1).text = "Address:"
+table.cell(4, 1).text = "Issue:"
+table.cell(7, 1).text = "Work Done:"
+table.cell(10, 1).text = "Misc Notes:"
+table.cell(11, 1).text = "To Invoice:"
 
 document.save('test.docx')
